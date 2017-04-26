5696d5bd298f62616cd5d30a0b5c50e3a5259a9a
Deepak Sharma <deepak.dev78@outlook.com>
2017-04-27 02:28:57 +1200

    - Calculate average of any numbers of item

--- AverageCalculator/Program.cs
+++ AverageCalculator/Program.cs
@@ -10,6 +10,25 @@ namespace AverageCalculator
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Average Calculator\n");
+            Console.Write("Enter number of items to be calculated for average: ");
+            int numberOfItems = int.Parse(Console.ReadLine());
+
+            double[] numberArray = new double[numberOfItems];
+            double sum = 0;
+
+            for (int i = 0; i < numberOfItems; i++)
+            {
+                Console.Write("Enter number {0} : ", i+1);
+                numberArray[i] = double.Parse(Console.ReadLine());
+                sum += numberArray[i];
+            }
+
+            double average = sum / numberOfItems;
+            Console.WriteLine("\nAverage is: {0}", average);
+
+            Console.ReadLine();
+
         }
     }
 }
